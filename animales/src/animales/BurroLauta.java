5cb76e164855df0bb786df113fe478247681cb74
MandaleCuete <lauty.monzalvo@gmail.com>
2021-10-14 12:15:10 -0300

    Commit lauta 1

---
+++ animales/src/animales/BurroLauta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animales;
+
+/**
+ *
+ * @author Porygon-Z
+ */
+public class BurroLauta {
+    public String nombre;
+    public int tamañoPo;
+    public String sexo;
+    
+}
